26192c61f6627ea924ab39fc1a0eb5d970d7e763
Benjamin LaGreca <benlagreca02@gmail.com>
2024-03-01 00:04:54 -0500

    Fixed play/pause hackishly, brightness broken for some reason, but script works fine, next is emojis

--- config.def.h
+++ config.def.h
@@ -69,6 +69,8 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+#define STATUSBAR "dwmblocks"
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
@@ -132,11 +134,13 @@ static const Key keys[] = {
 
     { 0, XF86XK_AudioPrev,	    	spawn,		SHCMD("playerctl previous") },
 	{ 0, XF86XK_AudioNext,	    	spawn,		SHCMD("playerctl next") },
-	{ 0, XF86XK_AudioPause,	    	spawn,		SHCMD("playerctl pause") },
-	{ 0, XF86XK_AudioPlay,	     	spawn,		SHCMD("playerctl play") },
+	{ 0, XF86XK_AudioPause,	    	spawn,		SHCMD("playerctl play-pause") },
+	{ 0, XF86XK_AudioPlay,	     	spawn,		SHCMD("playerctl play-pause") },
 
-    { 0, XF86XK_MonBrightnessUp,    spawn,      SHCMD("bright Up") },
-    { 0, XF86XK_MonBrightnessDown,  spawn,      SHCMD("bright Down") },
+    // broken for some reason? confirmed working a while ago but suddenly
+    // stopped...
+    { 0, XF86XK_MonBrightnessUp,    spawn,      SHCMD("bright +") },
+    { 0, XF86XK_MonBrightnessDown,  spawn,      SHCMD("bright -") },
 
 };
 
@@ -147,7 +151,9 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
+	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
+	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
